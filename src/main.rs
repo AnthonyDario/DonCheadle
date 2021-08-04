553dfe90af6fe35a18d5e60e69d2d4f413555097
Anthony Dario <darioad@amazon.com>
2021-08-04 01:16:14 -0700

    Clean up the header
    Made the header into a struct that contains an enum for the name. This fits
    better with a header's data being the same and a piece of that being the "name".

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     let host = args[1].clone();
 
     let content = match get_content(host) {
-        Ok(c) => c,
+        Ok(content) => content,
         Err(e) => {
             println!("Error: {:?}", e);
             return;
@@ -91,13 +91,19 @@ impl Response {
     }
 }
 
-enum Header {
-    Input(u8, String),                     // Input prompt
-    Success(u8, String),                   // MIME media type
-    Redirect(u8, String),                  // The Redirect Url
-    TemporaryFailure(u8, String),          // User facing error message
-    PermanentFailure(u8, String),          // User facing error message
-    ClientCertificateRequired(u8, String), // Error message
+struct Header {
+    name: HeaderName,
+    code: u8,
+    meta: String,
+}
+
+enum HeaderName {
+    Input,                     // Input prompt
+    Success,                   // MIME media type
+    Redirect,                  // The Redirect Url
+    TemporaryFailure,          // User facing error message
+    PermanentFailure,          // User facing error message
+    ClientCertificateRequired, // Error message
 }
 
 impl Header {
@@ -114,29 +120,26 @@ impl Header {
             .ok_or("No meta found in header")?
             .to_string();
 
-        match status_code {
-            10..=19 => Ok(Header::Input(status_code, meta)),
-            20..=29 => Ok(Header::Success(status_code, meta)),
-            30..=39 => Ok(Header::Redirect(status_code, meta)),
-            40..=49 => Ok(Header::TemporaryFailure(status_code, meta)),
-            50..=59 => Ok(Header::PermanentFailure(status_code, meta)),
-            60..=69 => Ok(Header::ClientCertificateRequired(status_code, meta)),
-            _ => Err(format!("Unexpected status code: {}", status_code)),
-        }
+        let name = match status_code {
+            10..=19 => HeaderName::Input,
+            20..=29 => HeaderName::Success,
+            30..=39 => HeaderName::Redirect,
+            40..=49 => HeaderName::TemporaryFailure,
+            50..=59 => HeaderName::PermanentFailure,
+            60..=69 => HeaderName::ClientCertificateRequired,
+            _ => return Err(format!("Unexpected status code: {}", status_code)),
+        };
+
+        Ok(Header {
+            name: name,
+            meta: meta,
+            code: status_code,
+        })
     }
 }
 
 impl fmt::Display for Header {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        // Is there really not a more succinct way to do this?
-        // Perhaps this means a struct would be better suited than an Enum...
-        match self {
-            Header::Input(code, meta) => write!(f, "{} {}\r\n", code, meta),
-            Header::Success(code, meta) => write!(f, "{} {}\r\n", code, meta),
-            Header::Redirect(code, meta) => write!(f, "{} {}\r\n", code, meta),
-            Header::TemporaryFailure(code, meta) => write!(f, "{} {}\r\n", code, meta),
-            Header::PermanentFailure(code, meta) => write!(f, "{} {}\r\n", code, meta),
-            Header::ClientCertificateRequired(code, meta) => write!(f, "{} {}\r\n", code, meta),
-        }
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(formatter, "{} {}\r\n", self.code, self.meta)
     }
 }
